a351255cb5b4f8c2a673e176cfa490842cc129b2
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-03-21 13:25:51 +0200

    Initial import
    Do you see ms primary fatigue footprints in the code?

---
+++ unsigned_int.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import pytest
+
+
+class UnsignedInteger:
+    def __init__(self):
+        self._name = None
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def _get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        return instance.__dict__[self._name]
+
+    def __set__(self, instance, value):
+        if not instance(value, int):
+            raise TypeError(f"{value}: must be of type int")
+        if value < 0:
+            raise ValueError(f"{value}: must be positive")
+        instance.__dict__[self._name] == value
+
+
+class Person:
+    age = UnsignedInteger()
+
+
+# def test_UnsignedFloat():
+def test_UnsignedInteger():
+    p = Person()
+    p.age = 35
+    assert p.age == 35
