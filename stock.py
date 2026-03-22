47348d4650bdda6e22b0e9f4738248332cce7836
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-03-22 09:40:09 +0200

    Finish OK test
    shares, name, price

--- stock.py
+++ stock.py
@@ -19,16 +19,20 @@ class Descriptor:
         return instance.__dict__[self._name]
 
     def __set__(self, instance, value):
+        self.validate(instance, value)
         instance.__dict__[self._name] = value
+
+    def validate(self, instance, value):
+        return value
 
 
 class Typed(Descriptor):
     _type = type(None)
 
-    def __set__(self, instance, value):
+    def validate(self, instance, value):
         if not isinstance(value, self._type):
             raise TypeError(f"{value}: expected {self._type}")
-        super().__set__(instance, value)
+        super().validate(instance, value)
 
 
 class Integer(Typed):
@@ -43,34 +47,37 @@ class String(Typed):
     _type = str
 
 
-class UnsignedInteger(Integer):
-    def __set__(self, instance, value):
+class Unsigned(Descriptor):
+    def validate(self, instance, value):
         if value < 0:
             raise ValueError(f"{value}: must be non negative")
-        super().__set__(instance, value)
+        super().validate(instance, value)
 
 
-class UnsignedFloat(Float):
-    def __set__(self, instance, value):
-        if value < 0:
-            raise ValueError(f"{value}: must be non negative")
-        super().__set__(instance, value)
+class UnsignedInteger(Integer, Unsigned):
+    pass
 
 
-class SizedString(String):
+class UnsignedFloat(Float, Unsigned):
+    pass
+
+
+class Bounded(Descriptor):
+    def validate(self, instance, value):
+        if len(value) < self.min_len or len(value) > self.max_len:
+            raise ValueError(
+                f"{value}: value must be in range [{self.min_len}..{self.max_len}]"
+            )
+        super().validate(instance, value)
+
+
+class SizedString(String, Bounded):
     def __init__(self, **opt):
         if "min_len" not in opt:
             opt["min_len"] = 0
         if "max_len" not in opt:
             raise ValueError("max_len is missing")
         super().__init__(**opt)
-
-    def __set__(self, instance, value):
-        if len(value) < self.min_len or len(value) > self.max_len:
-            raise ValueError(
-                f"{value}: length must be in range [{self.min_len}..{self.max_len}]"
-            )
-        super().__set__(instance, value)
 
 
 class Stock:
@@ -89,14 +96,3 @@ def test_stock():
     assert s.shares == 50
     assert s.name == "ACME"
     assert s.price == 91.1
-    with pytest.raises(ValueError) as exc:
-        s.shares = (x := -10)
-    assert str(exc.value) == f"{x}: must be non negative"
-    s.name = "ABRA"
-    assert s.name == "ABRA"
-    with pytest.raises(ValueError) as exc:
-        s.name = (t := "ABRACADABRAABRACAD")
-    assert str(exc.value) == f"{t}: length must be in range [0..12]"
-    with pytest.raises(TypeError) as exc:
-        s.shares = (t := "too much")
-    assert str(exc.value) == f"{t}: "
